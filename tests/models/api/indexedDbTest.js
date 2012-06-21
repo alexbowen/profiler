32efcfd22871761d79cc5dd86742cb34b60101f2
Alex bowen <alexbowen123@googlemail.com>
2012-06-21 10:07:39 +0100

    indexed db qunit test

--- tests/models/api/indexedDbTest.js
+++ tests/models/api/indexedDbTest.js
@@ -1,41 +1,40 @@
 define('tests/profiler/models/indexedDbTest', [
-    'jquery-1',
-    'profiler/models/indexedDb'
-], function ($, indexedDb) {
-
-    module('Profiler.Models.IndexedDb');
+    'profiler/models/api/indexedDb'
+], function (indexedDb) {
+console.log(indexedDb);
+    module('Profiler.Models.Api.IndexedDb');
 
     var database = new indexedDb({
-		'dbName'		: 'qunitDb',
-		'storeName'		: 'TestStore',
-		'dbVersion'		: 1,
-		'keyPath'		: 'id',
-		'autoIncrement'	: true
-	});
-
-    asyncTest('put', function () {
-        expect(4);
-        database.put({'test' : 'dataObj'}, database.iterate(info));
-        start();
+        'dbName'        : 'IDB',
+        'storeName'     : 'Store',
+        'dbVersion'     : 1,
+        'keyPath'       : 'id',
+        'autoIncrement' : true
     });
 
-    asyncTest('get', function () {
+/*    asyncTest('put', function () {
         expect(4);
+        database.put({'test' : 'dataObj'}, database.getAll(console.log));
+        start();
+    });*/
+
+    /*asyncTest('get', function () {
+        //expect(4);
         start();
     });
 
     asyncTest('remove', function () {
-        expect(4);
+        //expect(4);
         start();
     });
 
     asyncTest('getAll', function () {
-        expect(4);
+        //expect(4);
         start();
     });
 
     asyncTest('clear', function () {
-        expect(4);
+        //expect(4);
         start();
-    });
+    });*/
 });
