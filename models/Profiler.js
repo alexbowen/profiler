04ce6469a9fe077a2879a0bc520162aa388455c4
Alex bowen <alexbowen123@googlemail.com>
2012-06-22 11:28:13 +0100

    removed duplicated file

--- models/Profiler.js
+++ models/Profiler.js
@@ -1,12 +1,21 @@
-define('profiler/models/Profiler', function () {
+define('profiler/models/api/Chart', function () {
 
     /**
-     * @exports profiler/models/Profiler
+     * @exports profiler/models/api/Chart
      * @requires jquery-1
      */
-    var Profiler = {
+    var Chart = {
 
         /**
+         * init
+         * @return {void}
+         */
+        init: function () {
+            YAHOO.widget.Chart.SWFURL = "http://yui.yahooapis.com/2.9.0/build/charts/assets/charts.swf";
+        },
+
+        /**
+         * registerFunction
          * @param  {string} name
          * @param  {[type]}
          * @return {void}
@@ -16,6 +25,7 @@ define('profiler/models/Profiler', function () {
         },
 
         /**
+         * getFunctionReport
          * @param  {string} name
          * @return {object}
          */
@@ -24,13 +34,38 @@ define('profiler/models/Profiler', function () {
         },
 
         /**
+         * unregisterFunction
          * @param  {string} name
          * @return {void}
          */
         unregisterFunction: function (name) {
             YAHOO.tool.Profiler.unregisterFunction(name);
+        },
+
+        /**
+         * plot
+         * @param  {object} data
+         * @param  {string} type
+         * @return {void}
+         */
+        plot: function (data, type) {
+
+            var myDataSource, myChart;
+            YAHOO.example.profile = data;
+
+            myDataSource = new YAHOO.util.DataSource(YAHOO.example.profile);
+
+            myDataSource.responseType = YAHOO.util.DataSource.TYPE_JSARRAY;
+            myDataSource.responseSchema = {
+                fields: [ "id", "name", "avg", "calls", "max", "min" ]
+            };
+
+            myChart = new YAHOO.widget.LineChart("profile-view", myDataSource, {
+                xField: "id",
+                yField: type
+            });
         }
     };
 
-    return Profiler;
+    return Chart;
 });
